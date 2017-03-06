7c5f3acc2179581ba92b7c7e5d37e41294e072c0
RuhanLi <talktothehand9@hotmail.com>
2017-03-06 14:08:04 -0500

    Create youtubeiframe.js

---
+++ js/youtubeiframe.js
@@ -0,0 +1,11 @@
+function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'Y6XRT0_fA0s',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
